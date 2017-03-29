fa42fc72b529834d9b9b608cb78e5b66855c8891
Gennady Azarenkov <gazarenkov@codenvy.com>
2017-03-29 19:43:36 +0300

    added lost config objects

---
+++ core/che-core-api-model/src/main/java/org/eclipse/che/api/core/model/workspace/config/MachineConfig2.java
@@ -0,0 +1,36 @@
+/*******************************************************************************
+ * Copyright (c) 2012-2017 Codenvy, S.A.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Codenvy, S.A. - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.che.api.core.model.workspace.config;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Machine configuration
+ *
+ * @author Alexander Garagatyi
+ */
+public interface MachineConfig2 {
+    /**
+     * Returns list of configured agents.
+     */
+    List<String> getAgents();
+
+    /**
+     * Returns mapping of references to configurations of servers deployed into machine.
+     */
+    Map<String, ? extends ServerConf2> getServers();
+
+    /**
+     * Returns attributes of resources of machine.
+     */
+    Map<String, String> getAttributes();
+}
